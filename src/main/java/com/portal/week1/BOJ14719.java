1e72ca9b068412ae953e3a791dfe1dfed5bdbba4
heaming <meetoohi@gmail.com>
2024-08-09 13:19:09 +0900

    Initial commit

---
+++ src/main/java/com/portal/week1/BOJ14719.java
@@ -0,0 +1,16 @@
+package com.portal.week1;
+
+/**
+ * @title boj14719
+ * @author hey._.mi
+ * @link https://www.acmicpc.net/problem/14719
+ * @since 2024.08.09
+ * @tag 구현
+ */
+public class BOJ14719 {
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+        }
+    }
+}
